25fcab51a1d7984b331dad91c7013f6504b1f818
Sergey Chepurnenko <programmerkun@gmail.com>
2022-06-02 02:36:43 +0300

    Render implementation for the taskbar

--- taskbar.js
+++ taskbar.js
@@ -6,6 +6,9 @@ const AppFavorites = imports.ui.appFavorites;
 const PopupMenu = imports.ui.popupMenu;
 const Main = imports.ui.main;
 const DND = imports.ui.dnd;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const { AppButton } = Me.imports.appButton;
 
 //#endregion imports
 
@@ -22,7 +25,7 @@ var Taskbar = GObject.registerClass(
             // hide default app button in the panel
             Main.panel.statusArea.appMenu.container.hide();
 
-            // set required fields
+            // set properties
             this._appSystem = Shell.AppSystem.get_default();
             this._settings = settings;
 
@@ -72,6 +75,7 @@ var Taskbar = GObject.registerClass(
 
         _setConfig() {
             this._config = {
+                showFavorites: true,
                 // index to display the taskbar in the panel
                 // display after Activities button by default
                 panelIndex: 1,
@@ -97,7 +101,140 @@ var Taskbar = GObject.registerClass(
         }
 
         _render() {
+
+            let taskbarAppsById = new Map();
+            let taskbarAppButtonsByAppId = new Map();
+
+            // add favorites if enabled
+
+            if (this._config.showFavorites) {
+
+                const favoriteApps = AppFavorites.getAppFavorites().getFavorites();
+
+                for (let i = 0, l = favoriteApps.length; i < l; ++i) {
+
+                    const app = favoriteApps[i];
+
+                    taskbarAppsById.set(app.get_id(), {
+                        app: app,
+                        isFavorite: true
+                    });
+                }
+            }
+
+            // add running apps
+
+            const runningApps = this._getRunningApps();
+
+            for (let i = 0, l = runningApps.length; i < l; ++i) {
+
+                const app = runningApps[i];
+                const appId = app.get_id();
+
+                if (taskbarAppsById.has(appId)) {
+                    continue;
+                }
+
+                taskbarAppsById.set(appId, {
+                    app: app,
+                    isFavorite: false
+                });
+            }
+
+            // validate existing items in the taskbar
+
+            const layoutActors = this._layout.get_children();
+
+            for (let i = 0, l = layoutActors.length; i < l; ++i) {
+
+                let actor = layoutActors[i];
+
+                // check if the app button should stay in the taskbar
+                if (actor instanceof AppButton && actor.app &&
+                        taskbarAppsById.has(actor.app.get_id())) {
+                    taskbarAppButtonsByAppId.set(actor.app.get_id(), {
+                        appButton: actor,
+                        position: taskbarAppButtonsByAppId.size
+                    });
+                    continue;
+                }
+
+                // remove unnecessary items from the taskbar
+                this._layout.remove_child(actor);
+                actor.destroy();
+                actor = null;
+            }
+
+
+            // update/create app buttons
+
+            const taskbarAppIds = [...taskbarAppsById.keys()];
+
+            for (let i = 0, l = taskbarAppIds.length; i < l; ++i) {
+                
+                const appId = taskbarAppIds[i];
+                const {app, isFavorite} = taskbarAppsById.get(appId);
+
+                // create new app buttons
+                if (!taskbarAppButtonsByAppId.has(appId)) {
+                    this._createAppButton(app, isFavorite, i);
+                    continue;
+                }
+
+                // for existing app buttons check if position has changed
+                const {appButton, position} = taskbarAppButtonsByAppId.get(appId);
+                
+                // position has changed so move the app button
+                if (position !== i) {
+                    this._layout.remove_child(appButton);
+                    this._layout.insert_child_at_index(appButton, i);
+                }
+
+            }
+
             this._layout.queue_relayout();
+        }
+
+        /**
+         * appSystem.get_running() is slow to update
+         * using implementation from Dash to Panel instead
+        */
+        _getRunningApps() {
+            const workspaceIndex = global.workspace_manager.get_active_workspace_index();
+            const tracker = Shell.WindowTracker.get_default();
+            const windows = global.get_window_actors();
+            
+            let result = [];
+
+            for (let i = 0, l = windows.length; i < l; ++i) {
+
+                const window = windows[i].metaWindow;
+                
+                // get windows from the current workspace only
+                // skip windows that skip taskbar
+                if (window.get_workspace().index() !== workspaceIndex ||
+                        window.skipTaskbar) {
+                    continue;
+                }
+
+                const app = tracker.get_window_app(window);
+
+                if (!app || result.indexOf(app) >= 0) {
+                    continue;
+                }
+
+                result.push(app);
+            }
+
+            return result;
+        }
+
+        _createAppButton(app, isFavorite, index) {
+            this._layout.insert_child_at_index(new AppButton(
+                app, isFavorite,
+                this._settings,
+                (appButton, event) => {} // TODO: implementation
+            ), index);
         }
 
         _rerender() {
