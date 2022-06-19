d411a72a315e237e6a385ff4850512d34e4e04e4
Sergey Chepurnenko <programmerkun@gmail.com>
2022-06-20 00:12:54 +0300

    Create a template page for settings UI.

---
+++ settings/pageTemplate.js
@@ -0,0 +1,56 @@
+const { Adw, Gio, GLib, GObject, Gtk } = imports.gi;
+
+// TODO
+const _ = (text) => {
+    return text;
+};
+
+var SettingsPageTemplate = GObject.registerClass(
+    class SettingsPageTemplate extends Adw.PreferencesPage {
+
+        _init(params) {
+            super._init({
+                title: _(params.title),
+                name: params.name,
+                icon_name: params.icon
+            });
+            this._settings = params.settings;
+        }
+
+        addGroup(title, options) {
+
+            let newGroup = new Adw.PreferencesGroup({
+                title: _(title)
+            });
+
+            this.add(newGroup);
+
+            options?.forEach(option => newGroup.add(option));
+
+            return newGroup;
+        }
+
+        createSwitch(title, settingsKey) {
+
+            let newSwitch = new Gtk.Switch({
+                valign: Gtk.Align.CENTER
+            });
+
+            newSwitch.set_active(this._settings.get_boolean(settingsKey));
+
+            newSwitch.connect('notify::active', (widget) => {
+                this._settings.set_boolean(settingsKey, widget.get_active());
+            });
+
+            let switchRow = new Adw.ActionRow({
+                title: _(title),
+                activatable_widget: newSwitch
+            });
+
+            switchRow.add_suffix(newSwitch);
+
+            return switchRow;
+        }
+
+    }
+);
