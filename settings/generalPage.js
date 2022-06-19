fe796bb68b2d36d554692ad1b9eaf9181b119776
Sergey Chepurnenko <programmerkun@gmail.com>
2022-06-19 13:54:32 +0300

    Add configuration options: Enable Tooltips, Enable Indicators, Enable Notification Badges.

--- settings/generalPage.js
+++ settings/generalPage.js
@@ -19,7 +19,6 @@ var GeneralPage = GObject.registerClass(
 
             // Taskbar
             this._createTaskbarGroup();
-
         }
 
         _createTaskbarGroup() {
@@ -36,15 +35,15 @@ var GeneralPage = GObject.registerClass(
                 valign: Gtk.Align.CENTER
             });
 
-            let showFavoritesRow = new Adw.ActionRow({
-                title: _('Show Favorites'),
-                activatable_widget: showFavoritesSwitch
-            });
-
             showFavoritesSwitch.set_active(this._settings.get_boolean('taskbar-show-favorites'));
     
             showFavoritesSwitch.connect('notify::active', (widget) => {
                 this._settings.set_boolean('taskbar-show-favorites', widget.get_active());
+            });
+
+            let showFavoritesRow = new Adw.ActionRow({
+                title: _('Show Favorites'),
+                activatable_widget: showFavoritesSwitch
             });
 
             showFavoritesRow.add_suffix(showFavoritesSwitch);
@@ -56,11 +55,6 @@ var GeneralPage = GObject.registerClass(
             let isolateWorkspacesSwitch = new Gtk.Switch({
                 valign: Gtk.Align.CENTER
             });
-    
-            let isolateWorkspacesRow = new Adw.ActionRow({
-                title: _('Isolate Workspaces'),
-                activatable_widget: isolateWorkspacesSwitch
-            });
 
             isolateWorkspacesSwitch.set_active(this._settings.get_boolean('taskbar-isolate-workspaces'));
 
@@ -68,9 +62,77 @@ var GeneralPage = GObject.registerClass(
                 this._settings.set_boolean('taskbar-isolate-workspaces', widget.get_active());
             });
 
+            let isolateWorkspacesRow = new Adw.ActionRow({
+                title: _('Isolate Workspaces'),
+                activatable_widget: isolateWorkspacesSwitch
+            });
+
             isolateWorkspacesRow.add_suffix(isolateWorkspacesSwitch);
 
             taskbarGroup.add(isolateWorkspacesRow);
+
+            // Enable Tooltips
+
+            let enableTooltipsSwitch = new Gtk.Switch({
+                valign: Gtk.Align.CENTER
+            });
+
+            enableTooltipsSwitch.set_active(this._settings.get_boolean('appbutton-enable-tooltips'));
+
+            enableTooltipsSwitch.connect('notify::active', (widget) => {
+                this._settings.set_boolean('appbutton-enable-tooltips', widget.get_active());
+            });
+
+            let enableTooltipsSwitchRow = new Adw.ActionRow({
+                title: _('Enable Tooltips'),
+                activatable_widget: enableTooltipsSwitch
+            });
+
+            enableTooltipsSwitchRow.add_suffix(enableTooltipsSwitch);
+
+            taskbarGroup.add(enableTooltipsSwitchRow);
+
+            // Enable Indicators
+
+            let enableIndicatorsSwitch = new Gtk.Switch({
+                valign: Gtk.Align.CENTER
+            });
+
+            enableIndicatorsSwitch.set_active(this._settings.get_boolean('appbutton-enable-indicators'));
+
+            enableIndicatorsSwitch.connect('notify::active', (widget) => {
+                this._settings.set_boolean('appbutton-enable-indicators', widget.get_active());
+            });
+
+            let enableIndicatorsSwitchRow = new Adw.ActionRow({
+                title: _('Enable Indicators'),
+                activatable_widget: enableIndicatorsSwitch
+            });
+
+            enableIndicatorsSwitchRow.add_suffix(enableIndicatorsSwitch);
+
+            taskbarGroup.add(enableIndicatorsSwitchRow);
+
+            // Enable Notification Badges
+
+            let enableNotificationBadgesSwitch = new Gtk.Switch({
+                valign: Gtk.Align.CENTER
+            });
+
+            enableNotificationBadgesSwitch.set_active(this._settings.get_boolean('appbutton-enable-notification-badges'));
+
+            enableNotificationBadgesSwitch.connect('notify::active', (widget) => {
+                this._settings.set_boolean('appbutton-enable-notification-badges', widget.get_active());
+            });
+
+            let enableNotificationBadgesSwitchRow = new Adw.ActionRow({
+                title: _('Enable Notification Badges'),
+                activatable_widget: enableNotificationBadgesSwitch
+            });
+
+            enableNotificationBadgesSwitchRow.add_suffix(enableNotificationBadgesSwitch);
+
+            taskbarGroup.add(enableNotificationBadgesSwitchRow);
         }
 
     }
