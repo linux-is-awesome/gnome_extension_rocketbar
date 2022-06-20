f09cb7d86be6149368226b99baac9f1ea521fbb9
Sergey Chepurnenko <programmerkun@gmail.com>
2022-06-21 01:21:17 +0300

    Add Behavior page options to configure shell tweaks.

--- settings/behaviorPage.js
+++ settings/behaviorPage.js
@@ -25,6 +25,26 @@ var BehaviorPage = GObject.registerClass(
                 this.createSwitch('Allow Drag and Drop', 'appbutton-enable-drag-and-drop'),
                 this.createSwitch('Scroll to cycle app windows', 'appbutton-enable-scroll')
             ]);
+
+            // Panel
+            this.addGroup('Panel', [
+                this.createSwitch('Scroll to change sound volume', 'panel-enable-scroll')
+            ]);
+
+            // Activities
+            this.addGroup('Activities', [
+                this.createSwitch('Right click to open Apps', 'activities-enable-click-override')
+            ]);
+
+            // Overview
+            this.addGroup('Overview', [
+                this.createSwitch('Click Overview empty space to close it or open Apps', 'overview-enable-empty-space-clicks')
+            ]);
+
+            // Hot Corner
+            this.addGroup('Hot Corner', [
+                this.createSwitch('Enable Fullscreen Hot Corner', 'hotcorner-enable-in-fullscreen')
+            ]);
         }
 
     }
