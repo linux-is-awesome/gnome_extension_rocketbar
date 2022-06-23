efd1deb63f317cd06c43e8b0091035550d4b283f
Sergey Chepurnenko <programmerkun@gmail.com>
2022-06-23 13:55:51 +0300

    New Favorites util to manage favorite apps having better performance.

---
+++ utils/favorites.js
@@ -0,0 +1,81 @@
+const { Shell } = imports.gi;
+const AppFavorites = imports.ui.appFavorites;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const { Connections } = Me.imports.utils.connections;
+
+var Favorites = class Favorites {
+
+    constructor() {
+        this._callback = null;
+        this._apps = null;
+        this._appFavorites = AppFavorites.getAppFavorites();
+        this._connections = new Connections();
+        this._connections.add(Shell.AppSystem.get_default(), 'installed-changed', () => this._handleInstalledChanged());
+        this._connections.add(this._appFavorites, 'changed', () => this._handleChanged());
+    }
+
+    connect(callback) {
+        this._callback = callback;
+        return this;
+    }
+
+    destroy() {
+        this._connections.destroy();
+    }
+
+    getApps() {
+
+        if (!this._apps) {
+            this._apps = this._appFavorites.getFavorites();
+        }
+
+        return this._apps;
+    }
+
+    moveAppToPosition(appId, position) {
+        this._appFavorites.moveFavoriteToPos(appId, position);
+    }
+
+    _handleChanged() {
+
+        if (this._apps && this._callback) {
+            this._callback();
+        }
+
+        this._apps = null;
+    }
+
+    _handleInstalledChanged() {
+
+        const oldAppIds = this._getAppIds();
+        const newAppIds = this._appFavorites._getIds().toString();
+
+        // nothing has changed
+        if (oldAppIds === newAppIds) {
+            return;
+        }
+
+        this._apps = null;
+
+        if (this._callback) {
+            this._callback();
+        }
+    }
+
+    _getAppIds() {
+
+        if (!this._apps) {
+            return '';
+        }
+
+        let result = [];
+
+        for (let i = 0, l = this._apps.length; i < l; ++i) {
+            result.push(this._apps.id);
+        }
+
+        return result.toString();
+    }
+
+}
