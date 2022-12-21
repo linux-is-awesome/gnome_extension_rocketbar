7e67a2ba9898517da2493e8ce3fb6525e62b65b1
Sergey Chepurnenko <programmerkun@gmail.com>
2022-12-21 13:44:18 +0300

    Initial enums.

---
+++ extension/ui/base/enums.js
@@ -0,0 +1,21 @@
+/** 
+ * @enum {string}
+ */
+var Event = {
+    Broadcast: 'broadcast',
+    Hover: 'notify::hover',
+    FocusIn: 'key-focus-in',
+    FocusOut: 'key-focus-out',
+    Destroy: 'destroy',
+    PositionChanged: 'notify::position',
+    Mapped: 'notify::mapped'
+}
+
+/** 
+ * @enum {string}
+ */
+var Type = {
+    Function: 'function',
+    String: 'string',
+    Number: 'number'
+}
