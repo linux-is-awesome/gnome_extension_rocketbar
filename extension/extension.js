4ef8b0376c69910990400ed80a32e7089c020c47
Sergey Chepurnenko <programmerkun@gmail.com>
2022-12-21 14:07:55 +0300

    Redesign extension.js.

--- extension/extension.js
+++ extension/extension.js
@@ -1,30 +1,13 @@
-//#region imports
-
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const { Modules } = Me.imports.main.modules;
-
-//#endregion imports
-
-//#region variables
 
 let modules = null;
 
-//#endregion variables
-
-//#region main
-
-function init() {
-    ExtensionUtils.initTranslations();
-}
-
-function enable() {
-    modules = new Modules();
-}
-
-function disable() {
-    modules?.destroy();
-    modules = null;
-}
-
-//#endregion main
+var init = () => ExtensionUtils.initTranslations(),
+    enable = () => {
+        const { Modules } = ExtensionUtils.getCurrentExtension().imports.main.modules;
+        modules = new Modules();
+    },
+    disable = () => {
+        modules?.destroy();
+        modules = null;
+    };
