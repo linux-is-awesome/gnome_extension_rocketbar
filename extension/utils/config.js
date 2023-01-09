6eec0edcf98ab5b16411e947bf74275a6f7c8347
Sergey Chepurnenko <programmerkun@gmail.com>
2023-01-09 17:36:21 +0300

    Config module redesign.

--- extension/utils/config.js
+++ extension/utils/config.js
@@ -1,45 +1,51 @@
 /* exported Config */
 
-var Config = class {
+const Extension = imports.ui.extensionSystem.rocketbar;
 
-    constructor(callback = () => null) {
-        this._callback = callback;
-        this._old = null;
-        this.values = null; // { field => value }
-        this.update();
+const { GLib } = imports.gi;
+const { Context } = Extension.imports.core.context;
+const { Type } = Extension.imports.core.enums;
+
+/**
+ * @param {GLib.Variant} value 
+ * @returns {string|number|boolean}
+ */
+const translateValue = (value) => ({
+    s: () => value.get_string()?.toString()?.replace(/,[^,]+$/, ''),
+    b: () => value.get_boolean(),
+    i: () => value.get_int32()
+})[value?.get_type_string()]?.call();
+
+/**
+ * @typedef {string} settingsKey
+ * @typedef {string|number|boolean} value
+ * 
+ * @param {*} source
+ * @param {Object.<string, string> & {fieldName: settingsKey}} fields
+ * @param {(settingsKey: string) => void} [callback]
+ * @returns {Object.<string, string|number|boolean> & {fieldName: value}}
+ */
+var Config = (source, fields, callback) => {
+    if (!source || !fields) return null;
+    /** @type {Gio.Settings} */
+    const settings = Context.settings;
+    /** @type {Object.<string, string|number|boolean>} */
+    const values = {};
+    /** @type {Array<string>} */
+    const signals = [];
+    /** @type {Map<string, string>}*/
+    const valueMapping = new Map();
+    for (const fieldName in fields) {
+        const settingsKey = fields[fieldName];
+        if (typeof settingsKey !== Type.String) continue;
+        valueMapping.set(settingsKey, fieldName);
+        values[fieldName] = translateValue(settings.get_value(settingsKey));
+        signals.push(`changed::${settingsKey}`);
     }
-
-    update() {
-
-        if (!this._callback) {
-            return;
-        }
-
-        this._old = this.values;
-        this.values = this._callback();
-    }
-
-    hasOld() {
-        return !!this._old;
-    }
-
-    handleChanged(fields = [], callback = () => {}) {
-
-        if (!this._old) {
-            callback();
-            return;
-        }
-
-        if (!this.values) {
-            return;
-        }
-
-        for (let field of fields) {
-            if (this._old[field] !== this.values[field]) {
-                callback();
-                return;
-            }
-        }
-    }
-
-}
+    if (!signals.length) return values;
+    Context.signals.add(source, [[settings, signals, (_, key) => {
+        values[valueMapping.get(key)] = translateValue(settings.get_value(key));
+        if (typeof callback === Type.Function) callback(key);
+    }]]);
+    return values;
+};
