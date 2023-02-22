a7560eda8c84dd6bd5be5418cf3794f3dff6ccd5
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-02-22 11:48:08 +0300

    LongPressAction: call fake_release for St.Buttons

--- extension/ui/base/longPressAction.js
+++ extension/ui/base/longPressAction.js
@@ -13,17 +13,25 @@ export class LongPressAction {
     /** @type {number} */
     #delay = DEFAULT_LONG_PRESS_DELAY;
 
+    /** @type {St.Widget} */
+    #actor = null;
+
     /** @type {() => void} */
     #callback = null;
 
+    /**
+     * @param {St.Widget|Component} actor
+     * @param {() => void} callback
+     */
     constructor(actor, callback) {
         if (actor instanceof Component) {
             actor = actor.actor;
         }
         if (actor instanceof St.Widget === false || typeof callback !== Type.Function) return;
+        this.#actor = actor;
         this.#callback = callback;
         Context.signals.add(this, [
-            actor,
+            this.#actor,
             Event.ButtonPress, () => this.#handlePress(),
             Event.ButtonRelease, () => this.#handleRelease(),
             Event.Leave, () => this.#handleRelease()
@@ -33,13 +41,19 @@ export class LongPressAction {
     destroy() {
         Context.signals.removeAll(this);
         Context.jobs.removeAll(this);
+        this.#actor = null;
         this.#callback = null;
     }
 
     #handlePress() {
         if (typeof this.#callback !== Type.Function) return Clutter.EVENT_PROPAGATE;
-        Context.jobs.new(this, this.#delay).destroy(() => this.#callback()).catch();
+        Context.jobs.new(this, this.#delay).destroy(() => this.#handleDelay()).catch();
         return Clutter.EVENT_PROPAGATE;
+    }
+
+    #handleDelay() {
+        if (this.#actor instanceof St.Button) this.#actor.fake_release();
+        this.#callback()
     }
 
     #handleRelease() {
