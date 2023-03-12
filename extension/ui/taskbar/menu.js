1e1ee56524f50528dd636caafdbdb36020a40425
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-03-12 21:35:49 +0300

    AppMenu: Customize section initial implementation, MenuSection new features

--- extension/ui/taskbar/menu.js
+++ extension/ui/taskbar/menu.js
@@ -10,10 +10,13 @@ import { Context } from '../../core/context.js';
 import { Event, Type } from '../../core/enums.js';
 import { ComponentLocation } from '../base/component.js';
 import { Config } from '../../utils/config.js';
+import { ActivateBehavior, DemandsAttentionBehavior } from '../../utils/taskbar/appConfig.js';
+import { Animation, AnimationType, AnimationDuration } from '../base/animation.js';
 import { Labels } from '../../core/labels.js';
 
 const UNWANTED_STYLE_CLASS = 'app-menu';
 const DEFAULT_STYLE_CLASS = 'rocketbar__popup-menu';
+const SECTION_TITLE_STYLE_CLASS = 'rocketbar__popup-menu_section-title';
 const SLIDER_ICON_STYLE_CLASS = 'popup-menu-icon';
 
 /** @type {Object.<string, boolean>} */
@@ -42,6 +45,18 @@ const SliderValueProps = {
 const MenuPosition = {
     [ComponentLocation.Top]: St.Side.TOP,
     [ComponentLocation.Bottom]: St.Side.BOTTOM
+};
+
+/** @type {Object.<string, string>} */
+const ActivateBehaviorCheckboxGroup = {
+    [ActivateBehavior.NewWindow]: 'New Window',
+    [ActivateBehavior.MoveWindows]: 'Move Windows'
+};
+
+/** @type {Object.<string, string>} */
+const DemandsAttentionBehaviorCheckboxGroup = {
+    [DemandsAttentionBehavior.FocusActive]: 'Set Focus When Active',
+    [DemandsAttentionBehavior.FocusAll]: 'Always Set Focus'
 };
 
 /** @enum {string} */
@@ -93,8 +108,8 @@ class SliderMenuItem {
     }
 
     /**
-     * @param {(sender: SliderMenuItem) => void} callback
-     * @param {string} icon
+     * @param {(menuItem: SliderMenuItem) => void} callback
+     * @param {string} [icon]
      */
     constructor(callback, icon) {
         this.#actor.setOrnament(Ornament.HIDDEN);
@@ -114,6 +129,9 @@ class MenuSection {
     /** @type {PopupSubMenuMenuItem} */
     #actor = null;
 
+    /** @type {Map<PopupBaseMenuItem, number>} */
+    #hiddenMenuItems = null;
+
     /** @type {PopupSubMenuMenuItem} */
     get actor() {
         return this.#actor;
@@ -130,10 +148,77 @@ class MenuSection {
     }
 
     /**
-     * @param {string} title
+     * @param {string} [title]
+     * @param {boolean} [dominate]
      */
-    constructor(title) {
+    constructor(title, dominate = false) {
         this.#actor = new PopupSubMenuMenuItem(title);
+        this.#actor.connect(Event.Destroy, () => { this.#actor = null; });
+        if (!dominate) return;
+        this.menu?.connect(Event.OpenStateChanged, () => this.#handleState());
+    }
+
+    /**
+     * @param {string} [title]
+     */
+    addSeparator(title = null) {
+        if (!this.#actor?.menu) return;
+        const separator = new PopupSeparatorMenuItem(title);
+        if (title) separator.add_style_class_name(SECTION_TITLE_STYLE_CLASS);
+        this.#actor.menu.addMenuItem(separator);
+    }
+
+    /**
+     * @param {Object.<string, string>} items
+     * @param {(value: string, items: Object.<string, string>) => void} callback
+     * @returns {(value: string) => void}
+     */
+    addCheckboxGroup(items, callback) {
+        const menu = this.menu;
+        const group = new Map();
+        /** @param {string} value */
+        const handler = value => {
+            for (const [itemValue, item] of group) {
+                if (itemValue === value) item.setOrnament(Ornament.DOT);
+                else item.setOrnament(Ornament.NONE);
+            }
+            if (typeof callback === Type.Function) callback(value, items);
+        };
+        for (const value in items) group.set(value, menu.addAction(items[value], () => handler(value)));
+        return handler;
+    }
+
+    #handleState() {
+        if (!this.isOpen) {
+            if (!this.#hiddenMenuItems?.size) return;
+            const animationParams = { ...AnimationType.OpacityMax, ...{ mode: Clutter.AnimationMode.EASE_OUT_QUAD } };
+            for (const [menuItem, height] of this.#hiddenMenuItems) {
+                menuItem.show();
+                if (height < 0) continue;
+                Animation(menuItem, AnimationDuration.Faster, { ...animationParams, ...{ height } }).then(() => menuItem.set_size(-1, -1));
+            }
+            return this.#hiddenMenuItems.clear();
+        }
+        const parent = this.#actor?.get_parent();
+        if (!parent) return;
+        if (!this.#hiddenMenuItems) {
+            this.#hiddenMenuItems = new Map();
+        }
+        const menuItems = parent.get_children();
+        const animationParams = { ...AnimationType.HeightMin, ...{ mode: Clutter.AnimationMode.EASE_OUT_QUAD } };
+        for (let i = 0, l = menuItems.length; i < l; ++i) {
+            const menuItem = menuItems[i];
+            if (menuItem === this.#actor) continue;
+            if (menuItem instanceof St.ScrollView || !menuItem.visible) continue;           
+            const menuItemHeight = menuItem instanceof PopupSeparatorMenuItem ? -1 : menuItem.height; 
+            this.#hiddenMenuItems.set(menuItem, menuItemHeight);
+            if (menuItemHeight < 0) {
+                menuItem.hide();
+                continue;
+            }
+            menuItem.set(AnimationType.OpacityMin);
+            Animation(menuItem, AnimationDuration.Default, animationParams).then(() => menuItem.hide());
+        }
     }
 
 }
@@ -144,10 +229,10 @@ class SoundVolumeControlSection extends MenuSection {
     #appButton = null;
 
     /** @type {SliderMenuItem} */
-    #inputVolumeSlider = new SliderMenuItem(sender => this.#setVolume(sender), SoundVolumeIcon.Input);
+    #inputVolumeSlider = new SliderMenuItem(menuItem => this.#setVolume(menuItem), SoundVolumeIcon.Input);
 
     /** @type {SliderMenuItem} */
-    #outputVolumeSlider = new SliderMenuItem(sender => this.#setVolume(sender), SoundVolumeIcon.Output);
+    #outputVolumeSlider = new SliderMenuItem(menuItem => this.#setVolume(menuItem), SoundVolumeIcon.Output);
 
     /** @type {boolean} */
     #isSyncing = false;
@@ -161,6 +246,7 @@ class SoundVolumeControlSection extends MenuSection {
         this.menu.addMenuItem(this.#inputVolumeSlider.actor);
         this.menu.addMenuItem(this.#outputVolumeSlider.actor);
         this.menu.connect(Event.OpenStateChanged, () => this.#syncVolume());
+        this.actor.connect(Event.Destroy, () => { this.#appButton = null; });
     }
 
     update() {
@@ -173,17 +259,17 @@ class SoundVolumeControlSection extends MenuSection {
     }
 
     /**
-     * @param {SliderMenuItem} sender
+     * @param {SliderMenuItem} menuItem
      */
-    #setVolume(sender) {
+    #setVolume(menuItem) {
         if (this.#isSyncing) return;
         const soundVolumeControl = this.#appButton?.soundVolumeControl;
         if (!soundVolumeControl) return;
-        const slider = sender?.slider;
+        const slider = menuItem?.slider;
         if (!slider) return;
-        if (sender === this.#inputVolumeSlider) {
+        if (menuItem === this.#inputVolumeSlider) {
             soundVolumeControl.inputVolume = slider.value;
-        } else if (sender === this.#outputVolumeSlider) {
+        } else if (menuItem === this.#outputVolumeSlider) {
             soundVolumeControl.outputVolume = slider.value;
         }
     }
@@ -196,6 +282,73 @@ class SoundVolumeControlSection extends MenuSection {
         this.#inputVolumeSlider.slider.value = soundVolumeControl.inputVolume;
         this.#outputVolumeSlider.slider.value = soundVolumeControl.outputVolume;
         this.#isSyncing = false;
+    }
+
+}
+
+class CustomizeSection extends MenuSection {
+
+    /** @type {AppButton} */
+    #appButton = null;
+
+    /** @type {SliderMenuItem} */
+    #iconSizeSlider = new SliderMenuItem(menuItem => this.#setIconSize(menuItem));
+
+    #activateBehavior = null;
+
+    #demandsAttentionBehavior = null;
+
+    /** @type {PopupMenuItem} */
+    #importIconItem = null;
+
+    /** @type {PopupMenuItem} */
+    #resetIconItem = null;
+
+    /** @type {PopupMenuItem} */
+    #resetAllItem = null;
+
+    /** @type {boolean} */
+    #isSyncing = false;
+
+    /**
+     * @param {AppButton} appButton
+     */
+    constructor(appButton) {
+        super(Labels.Customize, true);
+        this.#appButton = appButton;
+        this.#createItems();
+        this.menu.connect(Event.OpenStateChanged, () => this.#sync());
+        this.actor.connect(Event.Destroy, () => this.#destroy());
+    }
+
+    #destroy() {
+        this.#appButton = null;
+    }
+
+    #createItems() {
+        const menu = this.menu;
+        this.addSeparator(Labels.ActivateBehavior);
+        this.#activateBehavior = this.addCheckboxGroup(ActivateBehaviorCheckboxGroup, value => {});
+        this.addSeparator(Labels.DemandsAttentionBehavior);
+        this.#demandsAttentionBehavior = this.addCheckboxGroup(DemandsAttentionBehaviorCheckboxGroup, value => {});
+        this.addSeparator(Labels.IconSize);
+        menu.addMenuItem(this.#iconSizeSlider.actor);
+        this.addSeparator(Labels.CustomIcon);
+        this.#importIconItem = menu.addAction(Labels.CopyIconToClipboard, () => {});
+        this.#resetIconItem = menu.addAction(Labels.ResetToDefault, () => {});
+        this.addSeparator();
+        this.#resetAllItem = menu.addAction(Labels.ResetAllToDefault, () => {});
+    }
+
+    async #sync() {
+        if (!this.isOpen) return;
+        this.#importIconItem.reactive = false;
+        this.#resetIconItem.hide();
+        this.#resetAllItem.reactive = false;
+    }
+
+    #setIconSize(menuItem) {
+        menuItem.value = menuItem.slider.value * 100;
     }
 
 }
@@ -253,6 +406,7 @@ export class Menu extends AppMenu {
     #createSections() {
         this.#soundVolumeControlSection = new SoundVolumeControlSection(this.#appButton);
         this.addMenuItem(this.#soundVolumeControlSection.actor);
+        if (this.#hasValidAppId) this.addMenuItem(new CustomizeSection(this.#appButton).actor);
         this.addMenuItem(new PopupSeparatorMenuItem());
         this.moveMenuItem(this._quitItem, this.numMenuItems);
     }
