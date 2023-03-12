9a7caa9799ec8e2001660c28c9300cd404b32f81
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-03-12 21:32:13 +0300

    Update legacy imports, add new labels

--- extension/core/labels.js
+++ extension/core/labels.js
@@ -5,5 +5,13 @@ const _ = imports.misc.extensionUtils.gettext;
 /** @enum {string} */
 export const Labels = {
     Pin: _('Pin'),
-    SoundVolumeControl: _('Sound Volume Control')
+    SoundVolumeControl: _('Sound Volume Control'),
+    Customize: _('Customize'),
+    ActivationBehavior: _('Activation Behavior'),
+    DemandsAttentionBehavior: _('Demands Attention Behavior'),
+    CustomIcon: _('Custom Icon'),
+    IconSize: _('Icon Size'),
+    CopyIconToClipboard: _('Copy Icon To Clipboard'),
+    ResetToDefault: _('Reset To Default'),
+    ResetAllToDefault: _('Reset All To Default')
 };
