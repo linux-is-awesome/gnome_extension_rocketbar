041d0ca906746feac020245f9e1119a1d66b13b9
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-05-14 17:51:25 +0300

    Taskbar: initial Drag And Drop functionality, minor code improvements

--- extension/ui/taskbar.js
+++ extension/ui/taskbar.js
@@ -1,7 +1,9 @@
 /* exported Taskbar */
 
+import Shell from 'gi://Shell';
 import { Main } from '../core/legacy.js';
 import { Context } from '../core/context.js';
+import { Type, Event } from '../core/enums.js';
 import { ComponentEvent } from './base/component.js';
 import { ScrollView } from './base/scrollView.js';
 import { TaskbarClient } from '../services/taskbarService.js';
@@ -9,7 +11,131 @@ import { AppButton } from './taskbar/appButton.js';
 
 const MODULE_NAME = 'Rocketbar__Taskbar';
 
+/**
+ * @param {Component} competitor
+ * @param {Meta.Rectangle} [xyRect]
+ * @returns {{rect: Meta.Rectangle, competitor: Component}}
+ */
+const DropCompetitor = (competitor, xyRect) => {
+    const rect = competitor.rect;
+    if (xyRect) {
+        [rect.x, rect.y] = [xyRect.x, xyRect.y];
+    }
+    return { rect, competitor };
+};
+
+export class DragAndDropHandler {
+
+    /** @type {DropCompetitor[]} */
+    #slots = [];
+
+    /** @type {Component} */
+    #parent = null;
+
+    /** @type {AppButton} */
+    #candidate = null;
+
+    /** @type {number} */
+    #candidatePosition = -1;
+
+    /** @type {() => void} */
+    #destroyCallback = null;
+
+    /**
+     * @param {Component} parent
+     * @param {Component[]} competitors
+     * @param {() => void} destroyCallback
+     */
+    constructor(parent, competitors, destroyCallback) {
+        if (!parent || !competitors) return;
+        this.#parent = parent;
+        for (const competitor of competitors) {
+            if (!competitor.isValid) continue;
+            this.#slots.push(DropCompetitor(competitor));
+        }
+        if (typeof destroyCallback !== Type.Function) return;
+        this.#destroyCallback = destroyCallback;
+    }
+
+    destroy() {
+        Context.jobs.removeAll(this);
+        Context.signals.removeAll(this);
+        this.#slots = null;
+        this.#parent = null;
+        this.#candidate?.destroy();
+        this.#candidate = null;
+        if (typeof this.#destroyCallback === Type.Function) this.#destroyCallback();
+        this.#destroyCallback = null;
+    }
+
+    handleDrag(params) {
+        if (!params || !this.#slots) return;
+        const { target, actor } = params;
+        this.#watchDragActor(actor);
+        const x = this.#parent?.rect.x + params.x;
+        const slots = [];
+        for (let i = 0, l = this.#slots.length; i < l; ++i) {
+            if (!this.#slots[i].competitor?.isValid) continue;
+            slots.push(this.#slots[i]);
+        }
+        this.#slots = slots;
+        let slotRect = null;
+        let position = this.#slots.length;
+        if (this.#candidatePosition > position) {
+            this.#candidatePosition = position;
+        }
+        for (let i = 0, l = position; i < l; ++i) {
+            const rect = this.#slots[i].rect;
+            if (x < rect.x || x > rect.x + rect.width) continue;
+            position = i;
+            slotRect = rect;
+            break;
+        }
+        const competitorAtPosition = this.#slots[position]?.competitor;
+        const competitorBeforePosition = this.#slots[position - 1]?.competitor;
+        const lastSlotPosition = this.#slots.length - 1;
+        if (target === competitorBeforePosition && position === lastSlotPosition) {
+            position++;
+            slotRect = null;
+        } else if (target === competitorAtPosition || target === competitorBeforePosition) {
+            this.#candidate?.destroy();
+            this.#candidate = null;
+            return;
+        }
+        if (this.#candidate && (this.#candidatePosition === position ||
+                                competitorAtPosition === this.#candidate)) {
+            this.#candidatePosition = position;
+            return;
+        }
+        this.#candidatePosition = position;
+        this.#candidate?.destroy();
+        this.#candidate = new AppButton(target.app, true).setParent(this.#parent, position);
+        if (!slotRect) return;
+        this.#slots.splice(position, 0, DropCompetitor(this.#candidate, slotRect));       
+    }
+
+    handleDrop() {
+        
+    }
+
+    #watchDragActor(actor) {
+        if (Context.signals.hasClient(this)) return;
+        Context.signals.add(this, [actor, Event.Destroy, () => this.destroy()]);
+    }
+
+}
+
 export class Taskbar extends ScrollView {
+
+    /**
+     * @param {{event: string, params: *, target: *}} data
+     * @returns {void}
+     */
+    #notifyHandler = (data) => ({
+        [ComponentEvent.Destroy]: this.#destroy,
+        [ComponentEvent.Mapped]: () => Context.layout.queueAfterInit(this, () => this.#rerender()),
+        [ComponentEvent.DragOver]: () => this.#handleDragOver(data?.target, data?.params)
+    })[data?.event]?.call(this);
 
     /** @type {Map<Meta.Workspace, Set<Shell.App>>} */
     #runningApps = Context.getSessionCache(this.constructor.name);
@@ -20,32 +146,51 @@ export class Taskbar extends ScrollView {
     /** @type {TaskbarClient} */
     #service = new TaskbarClient(() => this.#rerender());
 
+    /** @type {DragAndDropHandler} */
+    #dndHandler = null;
+
     constructor() {
         super(MODULE_NAME);
-        this.connect(ComponentEvent.Notify, data => this.#handleNotify(data));
+        this.dropEvents = true;
+        this.connect(ComponentEvent.Notify, data => this.#notifyHandler(data));
         Context.layout.requestInit(this, () => this.#setParent());
-    }
-
-    #handleNotify({ event }) {
-        switch (event) {
-            case ComponentEvent.Mapped:
-                Context.layout.queueAfterInit(this, () => this.#rerender())
-                return;
-            case ComponentEvent.Destroy:
-                this.#destroy();
-            default: return;
-        }
     }
 
     #destroy() {
         Context.layout.removeClient(this);
         this.#service?.destroy();
+        this.#dndHandler?.destroy();
         this.#service = null;
         this.#appButtons = null;
+        this.#dndHandler = null;
     }
 
     #setParent() {
         this.setParent(Main.panel._leftBox, -1);
+    }
+    
+    /**
+     * 
+     * @param {*} target 
+     * @param {{x: number, y: number}} params
+     * @returns {?}
+     */
+    #handleDragOver(target, params) {
+        if (!params || target?.app instanceof Shell.App === false) return;
+        if (!this.#dndHandler) {
+            const competitors = [...this.#appButtons.values()];
+            this.#dndHandler = new DragAndDropHandler(this, competitors, () => this.#handleDragEnd());
+        }
+        this.#dndHandler.handleDrag({ target, ...params });
+    }
+
+    #handleDrop() {
+
+    }
+
+    #handleDragEnd() {
+        if (!this.isValid) return;
+        this.#dndHandler = null;
     }
 
     #rerender() {
@@ -69,8 +214,8 @@ export class Taskbar extends ScrollView {
         if (mergedAppButtons.size !== appButtons.size) {
             let i = -1;
             for (const [app, appButton] of mergedAppButtons) {
-                if (!appButton.isValid) continue;
-                i++; if (appButtons.has(app)) continue;
+                if (!appButton.isValid) continue; i++;
+                if (appButtons.has(app)) continue;
                 appButtons.set(app, appButton);
                 sortedAppButtons.splice(i, 0, appButton);
             }
