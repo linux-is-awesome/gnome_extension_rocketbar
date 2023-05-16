d4eb150020f0ab5720cea266bb97c76ebc0d0654
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-05-16 22:29:12 +0300

    Taskbar Splitter initial implementation

---
+++ extension/ui/taskbar/splitter.js
@@ -0,0 +1,12 @@
+/* exported Splitter */
+
+import St from 'gi://St';
+import { Component } from '../base/component.js';
+
+export class Splitter extends Component {
+
+    constructor() {
+        super(new St.Widget());
+    }
+
+}
