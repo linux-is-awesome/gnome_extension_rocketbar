2993bfe35ef642972c776f46b5284b7fae92aac7
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-05-31 18:30:41 +0300

    ScrollView: minor optimizations, add scrollTo function

--- extension/ui/base/scrollView.js
+++ extension/ui/base/scrollView.js
@@ -1,8 +1,12 @@
 /* exported ScrollView */
 
 import St from 'gi://St';
+import Clutter from 'gi://Clutter';
 import { Type, Event } from '../../core/enums.js';
 import { Component } from './component.js';
+import { AnimationDuration } from './animation.js';
+
+const FADE_EFFECT_NAME = 'fade';
 
 /** @type {Object.<string, boolean|number>} */
 const DefaultProps = {
@@ -14,12 +18,12 @@ const DefaultProps = {
 
 export class ScrollView extends Component {
 
-    /** @type {Component} */
-    #layout = new Component(new St.BoxLayout());
+    /** @type {St.BoxLayout} */
+    #layout = new St.BoxLayout();
 
     /** @type {St.BoxLayout} */
     get actor() {
-        return this.#layout?.actor;
+        return this.#layout;
     }
 
     /** @type {St.ScrollView} */
@@ -32,10 +36,38 @@ export class ScrollView extends Component {
      */
     constructor(name = null) {
         super(new St.ScrollView({ name, ...DefaultProps }));
+        super.actor.add_actor(this.#layout);
         this.#layout.connect(Event.Destroy, () => { this.#layout = null; });
-        this.#layout.setParent(super.actor);
         if (typeof name !== Type.String) return;
-        this.#layout.setProps({ name: `${name}.Layout` });
+        this.#layout.set_name(`${name}-Layout`);
+    }
+
+    /**
+     * @param {Component|St.Widget} actor
+     */
+    scrollTo(actor) {
+        if (!this.isMapped) return;
+        if (actor instanceof Component) {
+            actor = actor.actor;
+        }
+        if (actor instanceof St.Widget === false) return;
+        const scrollView = super.actor;
+        const adjustment = scrollView?.hscroll?.adjustment;
+        if (!adjustment) return;
+        const hfade = scrollView.get_effect(FADE_EFFECT_NAME);
+        const offset = hfade?.fade_margins.left ?? 0;
+        let box = actor.get_allocation_box();
+        let { x1, x2 } = box;
+        box = this.#layout.get_allocation_box();
+        x1 += box.x1;
+        x2 += box.x1;
+        let [value, _, upper, __, ___, pageSize] = adjustment.get_values();
+        if (x1 < value + offset) {
+            value = Math.max(0, x1 - offset);
+        } else if (x2 > value + pageSize - offset) {
+            value = Math.min(upper, x2 + offset - pageSize);
+        } else return;
+        adjustment.ease(value, { mode: Clutter.AnimationMode.EASE_OUT_QUAD, duration: AnimationDuration.Fast });
     }
 
 }
