4b45595debd11fc41f40941e5cd74a363dba0feb
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-06-08 18:28:41 +0300

    ScrollView: various fixes and major  improvements, remove obsolete code

--- extension/ui/base/scrollView.js
+++ extension/ui/base/scrollView.js
@@ -2,7 +2,8 @@
 
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
-import { Type, Event } from '../../core/enums.js';
+import { Context } from '../../core/context.js';
+import { Type, Event, Delay } from '../../core/enums.js';
 import { Component } from './component.js';
 import { Animation, AnimationDuration } from './animation.js';
 
@@ -26,6 +27,9 @@ export class ScrollView extends Component {
     /** @type {St.Adjustment} */
     #scroll = null;
 
+    /** @type {Job} */
+    #handleScrollJob = Context.jobs.new(this, Delay.Redraw);
+
     /** @type {St.BoxLayout} */
     get actor() {
         return this.#layout;
@@ -39,16 +43,9 @@ export class ScrollView extends Component {
     /** @type {St.Adjustment} */
     get scroll() {
         if (!this.#scroll) {
-            this.#scroll = super.actor?.hscroll?.adjustment
+            this.#scroll = this.area?.hscroll?.adjustment
         }
         return this.#scroll;
-    }
-
-    /** @param {boolean} value */
-    set horizontalFade(value) {
-        const className = `h${FADE_EFFECT_NAME}`;
-        if (value) super.actor.add_style_class_name(className);
-        else super.actor.remove_style_class_name(className);
     }
 
     /**
@@ -59,7 +56,8 @@ export class ScrollView extends Component {
         const area = this.area;
         area.add_actor(this.#layout);
         area.connect(Event.Destroy, () => this.#destroy());
-        this.scroll?.connect(Event.AdjustmentChanged, () => this.#handleScrollSize());
+        this.scroll?.connect(Event.AdjustmentChanged, () => this.#handleScrollJob.reset().then(() =>
+                                                            this.#handleScrollSize()).catch());
         if (typeof name !== Type.String) return;
         this.#layout.set_name(`${name}-Layout`);
     }
@@ -76,7 +74,7 @@ export class ScrollView extends Component {
         }
         if (actor instanceof St.Widget === false) return null;
         let { value, pageSize, upper } = this.#scroll;
-        if (pageSize >= upper) return null; 
+        if (pageSize >= ~~upper) return null; 
         const fadeEffect = this.area.get_effect(FADE_EFFECT_NAME);
         const allocation = actor.get_allocation_box();
         const { x1, x2 } = allocation;
@@ -110,16 +108,24 @@ export class ScrollView extends Component {
 
 
     #destroy() {
+        this.#handleScrollJob?.destroy();
+        this.#handleScrollJob = null;
         this.#layout = null;
         this.#scroll = null;
     }
 
+    /**
+     * Note: Calling update_fade_effect to remove fade effect from the scroll view.
+     *       It's not implemented in C++ code for some reason.
+     */
     #handleScrollSize() {
         if (!this.isValid || !this.scroll) return;
+        const area = this.area;
         const className = `h${FADE_EFFECT_NAME}`;
         const { pageSize, upper } = this.#scroll;
-        if (pageSize >= upper) super.actor.remove_style_class_name(className);
-        else super.actor.add_style_class_name(className);
+        if (pageSize < ~~upper) return area.add_style_class_name(className);
+        area.remove_style_class_name(className);
+        area.update_fade_effect(new Clutter.Margin());
     }
 
 }
