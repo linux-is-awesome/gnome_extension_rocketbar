18d081c3338f1b7b0662d541b473527fea64ba79
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-02-20 22:20:59 +0300

    context/modules: using Map instead of a generic object to store module instances. rename tweaks

--- extension/core/context/modules.js
+++ extension/core/context/modules.js
@@ -1,24 +1,30 @@
 import Context from '../context.js';
 import { Config } from '../../utils/config.js';
 
+const MODULE_INSTANCE_PLACEHOLDER = true;
+
 /** @type {{[field: string]: string}} */
 const Modules = {
-    TweakKillDash: '../../tweaks/killDash.js',
+    TweakOverviewKillDash: '../../tweaks/overviewKillDash.js',
+    TweakOverviewClicks: '../../tweaks/overviewClicks.js',
+    TweakFocusFilter: '../../tweaks/focusFilter.js',
     Taskbar: '../../ui/taskbar.js',
     NotificationCounter: '../../ui/notificationCounter.js'
 };
 
 /** @type {{[field: string]: string}} */
 const ConfigFields = {
-    TweakKillDash: 'overview-kill-dash',
+    TweakOverviewKillDash: 'overview-kill-dash',
+    TweakOverviewClicks: 'overview-empty-space-clicks',
+    TweakFocusFilter: 'focus-behavior-override',
     Taskbar: 'taskbar-enabled',
     NotificationCounter: 'notification-counter-enabled'
 };
 
 export default class ModulesManager {
 
-    /** @type {{[field: string]: *}?} */
-    #modules = {};
+    /** @type {Map<string, *>?} */
+    #modules = new Map();
 
     /** @type {Config} */
     #config = Config(this, ConfigFields, () => this.#update());
@@ -28,9 +34,10 @@ export default class ModulesManager {
     }
 
     destroy() {
-        Context.signals.removeAll(this);
         if (!this.#modules) return;
-        for (const moduleName in this.#modules) this.#destroyModule(moduleName);
+        Context.signals.removeAll(this);
+        const moduleNames = this.#modules.keys();
+        for (const moduleName of moduleNames) this.#destroyModule(moduleName);
         this.#modules = null;
     }
 
@@ -39,14 +46,13 @@ export default class ModulesManager {
         for (const moduleName in Modules) {
             const configValue = this.#config[moduleName];
             if (typeof configValue !== 'boolean') continue;
-            const module = this.#modules[moduleName];
+            const module = this.#modules.get(moduleName);
             if (module && configValue) continue;
             if (!module && configValue) {
                 this.#constructModule(moduleName);
                 continue;
             }
             this.#destroyModule(moduleName);
-            delete this.#modules[moduleName];
         }
     }
 
@@ -56,10 +62,10 @@ export default class ModulesManager {
     async #constructModule(moduleName) {
         if (!this.#modules) return;
         try {
-            this.#modules[moduleName] = import(Modules[moduleName]);
-            const module = await this.#modules[moduleName];
-            if (!this.#modules?.[moduleName]) return;
-            this.#modules[moduleName] = new module.default();
+            this.#modules.set(moduleName, MODULE_INSTANCE_PLACEHOLDER);
+            const module = await import(Modules[moduleName]);
+            if (!this.#modules?.has(moduleName)) return;
+            this.#modules.set(moduleName, new module.default());
         } catch (e) {
             Context.logError(`unable to construct module: ${moduleName}.`, e);
         }
@@ -69,12 +75,12 @@ export default class ModulesManager {
      * @param {string} moduleName
      */
     #destroyModule(moduleName) {
-        if (!this.#modules) return;
+        if (!this.#modules?.has(moduleName)) return;
         try {
-            const module = this.#modules[moduleName];
-            if (!module) return;
-            this.#modules[moduleName] = null;
-            module.destroy();
+            const module = this.#modules.get(moduleName);
+            this.#modules.delete(moduleName);
+            if (module === MODULE_INSTANCE_PLACEHOLDER) return;
+            module?.destroy();
         } catch (e) {
             Context.logError(`unable to destroy module: ${moduleName}.`, e);
         }
