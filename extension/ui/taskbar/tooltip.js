5db207ad815757d63ada6d571fa7e3c9fcb13cb2
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-03-02 19:45:29 +0300

    taskbar/tooltip: initial implementation

--- extension/ui/taskbar/tooltip.js
+++ extension/ui/taskbar/tooltip.js
@@ -1,68 +1,137 @@
+/**
+ * JSDoc types
+ *
+ * @typedef {import('./appButton.js').AppButton} AppButton
+ * @typedef {import('../../utils/config.js').Config} Config
+ */
+
 import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import Context from '../../core/context.js';
-import { Component, ComponentEvent } from '../base/component.js';
-import { Event } from '../../core/enums.js';
-import { Animation, AnimationDuration, AnimationType } from '../base/animation.js';
-import { Config } from '../../utils/config.js';
+import { Tooltip as BaseTooltip } from '../base/tooltip.js';
+import { ComponentEvent } from '../base/component.js';
+import { SharedConfig } from '../../utils/config.js';
 
-const DefaultProps = {
+const MODULE_NAME = 'Rocketbar__Taskbar_Tooltip';
+const CONFIG_PATH = 'taskbar';
+const LAYOUT_STYLE_CLASS = 'rocketbar__tooltip_info-layout';
+const WINDOW_TITLE_STYLE_CLASS = 'rocketbar__tooltip_window-title';
 
+/** @enum {string} */
+const ConfigFields = {
 };
 
+/** @type {{[prop: string]: *}} */
 const LayoutProps = {
-
+    style_class: LAYOUT_STYLE_CLASS,
+    vertical: true,
+    x_expand: true,
+    clip_to_allocation: true
 };
 
-class Tooltip extends Component {
+/** @type {{[prop: string]: *}} */
+const WindowTitleProps = {
+    style_class: WINDOW_TITLE_STYLE_CLASS
+};
 
-    #layout = new St.Widget(LayoutProps);
+/**
+ * @param {string?} title
+ * @param {string?} [appName]
+ * @returns {string?}
+ */
+const WindowTitleText = (title, appName) => {
+    if (!title || !appName) return title ?? null;
+    const endRegExp = new RegExp(` -(?=[^-]*$).*${appName}$`);
+    if (title.match(endRegExp)) {
+        title = title.replace(endRegExp, '');
+    } else {
+        title = title.replace(new RegExp(`^${appName} - `), '');
+    }
+    return title;
+};
 
+export class Tooltip extends BaseTooltip {
+
+    /** @type {SharedConfig?} */
+    static #sharedConfig = null;
+
+    /** @type {{[event: string]: () => *}?} */
+    #events = {
+        [ComponentEvent.Destroy]: () => this.#destroy()
+    };
+
+    /** @type {Config?} */
+    #config = this.#configProvider.getConfig(this);
+
+    /** @type {AppButton?} */
     #appButton = null;
 
-    get actor() {
-        return this.#layout;
+    /** @type {St.BoxLayout?} */
+    #layout = null;
+
+    /** @type {St.Label?} */
+    #appName = null;
+
+    /** @type {St.Label?} */
+    #windowTitle = null;
+
+    /** @type {SharedConfig} */
+    get #configProvider() {
+        Tooltip.#sharedConfig ??= new SharedConfig(ConfigFields, { path: CONFIG_PATH });
+        return Tooltip.#sharedConfig;
     }
 
-    constructor() {
-        super(St.Bin(DefaultProps));
-        super.actor.set_child(this.#layout);
-        this.#appButton = appButton;
-    }
-
-    destroy() {
-        this.#appButton = null;
-    }
-
-}
-
-export class TooltipTrigger {
-
-    static tooltip = null;
-
-    #appButton = null;
-
-    #tooltip = null;
-
-    // TODO: lazy loading!
-    #config = null;
-
+    /**
+     * @param {AppButton} appButton
+     */
     constructor(appButton) {
-        if (!appButton) return;
+        super(appButton, MODULE_NAME);
+        this.trackHover = true;
         this.#appButton = appButton;
-        Context.signals.add(this, [
-            this.#appButton.actor,
-            Event.Hover, () => { },
-            Event.FocusIn, () => { },
-            Event.FocusOut, () => { },
-            Event.ButtonPress, () => { }
-        ]); 
+        this.#appName = new St.Label();
+        this.#appName.set_text(appButton?.app?.get_name() ?? '');
+        this.#windowTitle = new St.Label(WindowTitleProps);
+        this.#layout = new St.BoxLayout(LayoutProps);
+        this.#layout.add_child(this.#windowTitle);
+        this.#layout.add_child(this.#appName);
+        this.actor.add_child(this.#layout);
+        this.connect(ComponentEvent.Notify, data => this.#events?.[data?.event]?.());
     }
 
-    destroy() {
-        Context.jobs.removeAll(this);
-        Context.signals.removeAll(this);
+    /**
+     * @override
+     */
+    rerender() {
+        if (this.isHidden) return;
+        this.lockSize();
+        this.#rerender();
+        super.rerender();
+    }
+
+    #destroy() {
         this.#appButton = null;
+        this.#layout = null;
+        this.#appName = null;
+        this.#windowTitle = null;
+        this.#config = null;
+        this.#events = null;
+        if (!Tooltip.#sharedConfig?.destroy(this)) return;
+        Tooltip.#sharedConfig = null;
+    }
+
+    #rerender() {
+        if (!this.#windowTitle || !this.#appButton) return;
+        let text = null;
+        const windows = this.#appButton.windows;
+        const appWindows = this.#appButton.app?.get_windows();
+        const appName = this.#appButton.app?.get_name();
+        if (windows?.size && appWindows?.length) {
+            for (const window of appWindows) {
+                if (!windows.has(window)) continue;
+                text = WindowTitleText(window.title, appName);
+                break;
+            }
+        }
+        const visible = !!text;
+        this.#windowTitle.set({ text, visible });
     }
 
 }
