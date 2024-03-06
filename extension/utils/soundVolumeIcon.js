8e98d2e113c80d51dd65020ca60b7262a8df21bb
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-03-06 18:53:46 +0300

    utils: Add SoundVolumeIcon

---
+++ extension/utils/soundVolumeIcon.js
@@ -0,0 +1,37 @@
+/** @enum {string} */
+const SoundOutputIcon = {
+    Muted: 'audio-volume-muted-symbolic',
+    Low: 'audio-volume-low-symbolic',
+    Medium: 'audio-volume-medium-symbolic',
+    High: 'audio-volume-high-symbolic'
+};
+
+/** @enum {string} */
+const SoundInputIcon = {
+    Muted: 'audio-volume-muted-symbolic',
+    Low: 'audio-volume-low-symbolic',
+    Medium: 'audio-volume-medium-symbolic',
+    High: 'audio-volume-high-symbolic'
+};
+
+/** @enum {number} */
+const SoundVolumeIconIndex = {
+    Muted: 0,
+    Low: 1,
+    Medium: 2,
+    High: 3
+};
+
+/**
+ * @param {number} volumeLevel positive float values 0..0.1...0.8..0.9..1
+ * @param {boolean} [isInput]
+ * @returns {string}
+ */
+export const SoundVolumeIcon = (volumeLevel, isInput = false) => {
+    const icons = isInput ? SoundInputIcon : SoundOutputIcon;
+    if (typeof volumeLevel !== 'number' ||
+        volumeLevel <= SoundVolumeIconIndex.Muted) return icons.Muted;
+    let iconIndex = Math.trunc(SoundVolumeIconIndex.High * volumeLevel + SoundVolumeIconIndex.Low);
+    iconIndex = Math.min(SoundVolumeIconIndex.High, Math.max(SoundVolumeIconIndex.Low, iconIndex));
+    return Object.values(icons)[iconIndex];
+};
