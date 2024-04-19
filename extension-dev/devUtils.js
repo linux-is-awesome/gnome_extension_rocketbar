6d392e7d6bd60b848cbdb3149cec4419831d7ec1
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-04-19 23:23:14 +0300

    extension-dev: remove JSON config support

--- extension-dev/devUtils.js
+++ extension-dev/devUtils.js
@@ -1,5 +1,4 @@
 import GLib from 'gi://GLib';
-import Gio from 'gi://Gio';
 
 /**
  * @param {string} path
@@ -12,19 +11,4 @@ export const RuntimeLocation = path => {
     const runtimeId = new TextDecoder().decode(output)?.trim() ?? null;
     if (!runtimeId) return null;
     return `/${runtimeId}`;
-};
-
-/**
- * @param {string} path
- * @returns {{[key: string]: *}}
- */
-export const JSONConfig = path => {
-    try {
-        const configFile = Gio.File.new_for_path(path);
-        const [success, contents] = configFile.load_contents(null);
-        return success && contents ? JSON.parse(new TextDecoder().decode(contents)) : {};
-    } catch (e) {
-        console.error(e);
-    }
-    return {};
 };
