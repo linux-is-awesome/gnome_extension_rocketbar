d37533dc8d1301cb731601504e88bc0ec77338c7
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-05-02 00:31:51 +0300

    tweaks/overviewClicks: handle search

--- extension/tweaks/overviewClicks.js
+++ extension/tweaks/overviewClicks.js
@@ -2,6 +2,7 @@
  * JSDoc types
  *
  * @typedef {import('resource:///org/gnome/shell/ui/overviewControls.js').ControlsManager} ControlsManager
+ * @typedef {import('resource:///org/gnome/shell/ui/searchController.js').SearchController} SearchController
  */
 
 import Clutter from 'gi://Clutter';
@@ -16,16 +17,20 @@ export default class {
     /** @type {ControlsManager?} */
     #overviewControls = null;
 
+    /** @type {SearchController?} */
+    #searchController = null;
+
     constructor() {
         const overviewControls = Overview._overview?._controls;
         if (!overviewControls ||
             typeof Overview.toggle !== 'function' ||
             typeof overviewControls?._toggleAppsPage !== 'function') return;
+        this.#searchController = Overview.searchController ?? null;
+        this.#overviewControls = overviewControls;
         this.#clickAction = new Clutter.ClickAction();
         this.#clickAction.connect(Event.Clicked, event => this.#handleClick(event));
         overviewControls.reactive = true;
         overviewControls.add_action(this.#clickAction);
-        this.#overviewControls = overviewControls;
     }
 
     destroy() {
@@ -34,12 +39,14 @@ export default class {
         this.#overviewControls.remove_action(this.#clickAction);
         this.#overviewControls = null;
         this.#clickAction = null;
+        this.#searchController = null;
     }
 
     /**
      * @param {Clutter.Event} event
      */
     #handleClick(event) {
+        if (this.#searchController?.searchActive) return this.#searchController.reset();
         switch (event?.get_button()) {
             case Clutter.BUTTON_PRIMARY:
                 Overview.toggle();
