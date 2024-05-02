38e681bff797914775dca2ca70b2e999afb4dca5
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2024-05-02 21:00:23 +0300

    tweaks/overviewKillDash: hide 'Show Apps' button on search screen

--- extension/tweaks/overviewKillDash.js
+++ extension/tweaks/overviewKillDash.js
@@ -1,10 +1,16 @@
+/**
+ * JSDoc types
+ *
+ * @typedef {import('resource:///org/gnome/shell/ui/searchController.js').SearchController} SearchController
+ */
+
 import Clutter from 'gi://Clutter';
 import St from 'gi://St';
 import { initializeDeferredWork as DeferredWork } from 'resource:///org/gnome/shell/ui/main.js';
 import { Overview } from '../core/shell.js';
 import Context from '../core/context.js';
 import { Event } from '../core/enums.js';
-import { Animation, AnimationDuration } from '../ui/base/animation.js';
+import { Animation, AnimationDuration, AnimationType } from '../ui/base/animation.js';
 
 const MODULE_NAME = 'Rocketbar__Tweak_OverviewKillDash';
 const STYLE_CLASS = 'rocketbar__tweak_overview-kill-dash';
@@ -28,8 +34,14 @@ export default class {
     /** @type {string?} */
     #dashWorkId = null;
 
+    /** @type {St.Button?} */
+    #showAppsButton = null;
+
     /** @type {{[key: string]: *}?} */
     #showAppsButtonDefaultProps = null;
+
+    /** @type {SearchController?} */
+    #searchController = null;
 
     constructor() {
         const dash = Overview.dash;
@@ -48,15 +60,22 @@ export default class {
         dash._box?.hide();
         dash.add_style_class_name(STYLE_CLASS);
         const showAppsButton = dash.showAppsButton;
-        if (!showAppsButton) return;
-        Context.signals.add(this, [showAppsButton, Event.Checked, () => this.#updateActorRotation()]);
+        const searchController = Overview.searchController;
+        if (!showAppsButton || !searchController) return;
+        Context.signals.add(this,
+            [showAppsButton, Event.Checked, () => this.#updateState()],
+            [searchController, Event.SearchActive, () => this.#updateState()]);
         let style_class = showAppsButton.get_style_class_name();
         let child = showAppsButton.get_child();
-        this.#showAppsButtonDefaultProps = { style_class, child };
+        let pivot_point = showAppsButton.pivot_point;
+        this.#showAppsButtonDefaultProps = { style_class, child, pivot_point };
         style_class = SHOW_APPS_BUTTON_STYLE_CLASS;
         child = this.#actor;
         showAppsButton.set({ style_class, child });
-        this.#updateActorRotation();
+        showAppsButton.set_pivot_point(0.5, 0.5);
+        this.#showAppsButton = showAppsButton;
+        this.#searchController = searchController;
+        this.#updateState();
     }
 
     destroy() {
@@ -69,20 +88,38 @@ export default class {
             dash._workId = this.#dashWorkId;
         }
         if (this.#showAppsButtonDefaultProps) {
-            dash.showAppsButton?.set(this.#showAppsButtonDefaultProps);
+            this.#showAppsButton?.set(this.#showAppsButtonDefaultProps);
         }
         this.#dashWorkId = null;
         this.#showAppsButtonDefaultProps = null;
+        this.#showAppsButton = null;
+        this.#searchController = null;
         dash.remove_style_class_name(STYLE_CLASS);
         dash._box?.show();
         dash._background?.show();
         dash._queueRedisplay();
     }
 
+    #updateState() {
+        if (!this.#actor || !this.#showAppsButton) return;
+        const isSearchActive = !!this.#searchController?.searchActive;
+        const duration = AnimationDuration.Fast;
+        const mode = Clutter.AnimationMode.EASE_OUT_QUAD;
+        if (isSearchActive) {
+            const animationParams = { ...AnimationType.ScaleMin, ...AnimationType.OpacityMin, mode };
+            Animation(this.#showAppsButton, duration, animationParams);
+            return;
+        }
+        if (this.#showAppsButton.opacity !== AnimationType.OpacityMax.opacity) {
+            const animationParams = { ...AnimationType.ScaleNormal, ...AnimationType.OpacityMax, mode };
+            Animation(this.#showAppsButton, duration, animationParams);
+        }
+        this.#updateActorRotation();
+    }
+
     #updateActorRotation() {
-        if (!this.#actor) return;
-        const showAppsButton = Overview.dash?.showAppsButton;
-        if (!showAppsButton) return;
+        if (!this.#actor || !this.#showAppsButton) return;
+        const showAppsButton = this.#showAppsButton;
         const rotation_angle_z = showAppsButton.checked ? ACTOR_ROTATION_CHECKED : ACTOR_ROTATION_DEFAULT;
         if (this.#actor.rotation_angle_z === rotation_angle_z) return;
         const mode = Clutter.AnimationMode.EASE_OUT_QUAD;
