d56bcbb96562de77c99449b52e6aebb85dda266b
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-01-14 19:09:44 +0300

    prefs: remove legacy code

--- extension/prefs.js
+++ extension/prefs.js
@@ -1,29 +1,15 @@
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const { GeneralPage } = Me.imports.settings.generalPage;
-const { CustomizePage } = Me.imports.settings.customizePage;
-const { BehaviorPage } = Me.imports.settings.behaviorPage;
-const { AboutPage } = Me.imports.settings.aboutPage;
+import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-function init() {
-    ExtensionUtils.initTranslations();
-}
+export default class extends ExtensionPreferences {
 
-function fillPreferencesWindow(window) {
-    const settings = ExtensionUtils.getSettings();
 
-    // enable search
-    window.set_search_enabled(true);
+    /**
+     * @override
+     * @param {Adw.PreferencesWindow} window
+     */
+    fillPreferencesWindow(window) {
+    }
 
-    // resize the window
-    window.set_size_request(
-        window.default_width + 50,
-        window.default_height + 150
-    );
 
-    // create pages
-    window.add(new GeneralPage(settings));
-    window.add(new CustomizePage(settings));
-    window.add(new BehaviorPage(settings));
-    window.add(new AboutPage());
+
 }
