1af19bee4999e7f58117ab43beed1470b59530a7
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-01-14 19:16:31 +0300

    preferences/labels: initial implementation

---
+++ extension/preferences/labels.js
@@ -0,0 +1,5 @@
+import { gettext } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import SharedLabels from '../shared/labels.js';
+
+/** @enum {string} */
+export const Labels = SharedLabels(gettext);
