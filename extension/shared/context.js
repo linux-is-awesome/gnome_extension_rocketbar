b1798fbfbe1c5b77c89138a225ed428e2116e2f5
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-01-16 20:35:54 +0300

    shared/context: general Context class initial implementation

---
+++ extension/shared/context.js
@@ -0,0 +1,152 @@
+/**
+ * @typedef {import('resource:///org/gnome/shell/extensions/sharedInternals').ExtensionBase} Extension
+ */
+
+import Jobs from './context/jobs.js';
+import Signals from './context/signals.js';
+import Settings from './settings.js';
+
+const SETTINGS_SCHEMA_KEY = 'settings-schema';
+
+/** @type {Context?} */
+let _instance = null;
+
+/** @type {Map<*, Map>?} a persistent cache to keep some data until the user's session ends or the extension gets disabled by the user */
+let _sessionStorage = null;
+
+export default class Context {
+
+    /** @type {Context} */
+    static get instance() {
+        if (!_instance) throw new Error(`${this.name} has no instance.`);
+        return _instance;
+    }
+
+    /** @type {{[key: string]: *}?} */
+    static get metadata() {
+        return this.instance.#extension?.metadata;
+    }
+
+    /** @type {string?} */
+    static get path() {
+        return this.instance.#extension?.path ?? null;
+    }
+
+    /** @type {Jobs} */
+    static get jobs() {
+        const instance = this.instance;
+        instance.#jobs ??= new Jobs();
+        return instance.#jobs;
+    }
+
+    /** @type {Signals} */
+    static get signals() {
+        const instance = this.instance;
+        instance.#signals ??= new Signals();
+        return instance.#signals;
+    }
+
+    /**
+     * @param {string?} [path]
+     * @returns {Settings?}
+     */
+    static getSettings(path) {
+        try {
+            const extension = this.instance.#extension;
+            if (!extension) return null;
+            const schemaId = path ? `${extension.metadata[SETTINGS_SCHEMA_KEY]}.${path}` : '';
+            const storage = this.getStorage(this.name);
+            if (storage.has(schemaId)) return storage.get(schemaId);
+            const settings = new Settings(extension.getSettings(schemaId));
+            storage.set(schemaId, settings);
+            return settings;
+        } catch (e) {
+            this.logError(`unable to load settings for the path ${path}.`);
+        }
+        return null;
+    }
+
+    /**
+     * @param {*} client
+     * @returns {Map}
+     */
+    static getStorage(client) {
+        if (!_instance) throw new Error(`${this.name} is invalid.`);
+        if (!client) throw new Error(`${this.name}.getStorage requires a client reference.`);
+        _sessionStorage ??= new Map();
+        const clientStorage = _sessionStorage.get(client) ?? new Map();
+        _sessionStorage.set(client, clientStorage);
+        return clientStorage;
+    }
+
+    /**
+     * @param {string?} [message]
+     * @param {*} [error]
+     */
+    static logError(message, error) {
+        console.error(`${this.metadata?.name ?? this.name} ${message ?? ''}`, error ?? '');
+    }
+
+    /** @type {Extension?} */
+    #extension = null;
+
+    /** @type {Jobs?} */
+    #jobs = null;
+
+    /** @type {Signals?} */
+    #signals = null;
+
+    /** @type {() => boolean} */
+    #destroyCallback = () => true;
+
+    /**
+     * @param {Extension} extension
+     * @param {() => boolean} [destroyCallback] return `true` to destroy the session storage, otherwise return `false`
+     */
+    constructor(extension, destroyCallback) {
+        if (_instance) throw new Error(`${Context.name} already has an instance.`);
+        if (!extension) throw new Error(`${Context.name} requires an instance of the extension class.`);
+        this.#extension = extension;
+        this.#destroyCallback = destroyCallback ?? this.#destroyCallback;
+        _instance = this;
+    }
+
+    destroy() {
+        try {
+            const callbackResult = this.#destroyCallback();
+            this.#jobs?.removeAll(this);
+            this.#jobs?.destroy();
+            this.#signals?.destroy();
+            this.#cleanSessionStorage(callbackResult);
+        } catch (e) {
+            Context.logError(`unable to destroy ${Context.name}.`, e);
+        } finally {
+            this.#jobs = null;
+            this.#signals = null;
+            this.#extension = null;
+            _instance = null;
+        }
+    }
+
+    /**
+     * @param {boolean} nullify
+     */
+    #cleanSessionStorage(nullify) {
+        if (!_sessionStorage) return;
+        if (nullify) {
+            _sessionStorage.clear();
+            _sessionStorage = null;
+            return;
+        }
+        _sessionStorage.delete(Context.name);
+        const clients = [..._sessionStorage.keys()];
+        for (let i = 0, l = clients.length; i < l; ++i) {
+            const client = clients[i];
+            if (_sessionStorage.get(client)?.size) continue;
+            _sessionStorage.delete(client);
+        }
+        if (_sessionStorage.size) return;
+        _sessionStorage = null;
+    }
+
+}
---
+++ extension/shared/context/jobs.js
@@ -0,0 +1,201 @@
+/**
+ * @typedef {Job} Jobs.Job
+ * @typedef {import('gi://Meta').Laters} Meta.Laters
+ */
+
+import GLib from 'gi://GLib';
+import Context from '../context.js';
+import { Delay } from '../enums.js';
+
+const LATER_TYPE_BEFORE_REDRAW = 1;
+const LATER_TYPE_IDLE = 2;
+
+/** @type {{[delay: string]: number}} */
+const LaterType = {
+    [Delay.Redraw]: LATER_TYPE_BEFORE_REDRAW,
+    [Delay.Idle]: LATER_TYPE_IDLE
+};
+
+/** @type {Meta.Laters} */
+const Laters = global.compositor.get_laters();
+
+class Job {
+
+    /** @type {number?} */
+    #id = null;
+
+    /** @type {Promise<boolean>?} */
+    #job = null;
+
+    /** @type {((isFinished: boolean) => void)?} */
+    #resolver = null;
+
+    /** @type {number?} */
+    #delay = null;
+
+    /** @type {((job: this) => void)?} */
+    #destroyCallback = null;
+
+    /** @type {boolean} */
+    get #isValid() {
+        return typeof this.#destroyCallback === 'function' &&
+               typeof this.#delay === 'number';
+    }
+
+    /** @type {Promise<boolean>?} */
+    get #currentJob() {
+        if (!this.#isValid) return null;
+        this.#job ??= new Promise(resolve => this.#start(resolve));
+        return this.#job;
+    }
+
+    /**
+     * @param {(job: Job) => void} destroyCallback
+     * @param {number?} [delay]
+     */
+    constructor(destroyCallback, delay) {
+        this.#destroyCallback = destroyCallback;
+        this.#delay = delay ?? Delay.Idle;
+    }
+
+    /**
+     * @param {(() => void)?} [callback] a function to call after destroy
+     * @returns {this}
+     */
+    destroy(callback) {
+        if (!this.#isValid) return this;
+        if (typeof callback === 'function') return this.queue(() => (this.destroy(), callback()));
+        this.#abort();
+        if (this.#destroyCallback) this.#destroyCallback(this);
+        this.#delay = null;
+        this.#destroyCallback = null;
+        return this;
+    }
+
+    /**
+     * @param {() => void} callback
+     * @returns {this}
+     */
+    queue(callback) {
+        if (typeof callback !== 'function') return this;
+        this.#job = this.#currentJob?.then(isFinished => {
+            if (isFinished) callback();
+            return isFinished;
+        }) ?? this.#job;
+        return this;
+    }
+
+    /**
+     * @param {number?} [delay]
+     * @returns {this}
+     */
+    reset(delay) {
+        this.#abort();
+        if (typeof delay !== 'number') return this;
+        this.#delay = delay;
+        return this;
+    }
+
+    /**
+     * @param {(isFinished: boolean) => void} resolver
+     */
+    #start(resolver) {
+        this.#resolver = resolver;
+        const delay = this.#delay ?? Delay.Idle;
+        this.#id = delay === Delay.Redraw || delay === Delay.Idle ?
+                   Laters.add(LaterType[delay], () => this.#finish()) :
+                   GLib.timeout_add(GLib.PRIORITY_DEFAULT_IDLE, delay, () => this.#finish());
+    }
+
+    #abort() {
+        if (this.#id) {
+            if (this.#delay === Delay.Redraw || this.#delay === Delay.Idle) Laters.remove(this.#id);
+            else GLib.source_remove(this.#id);
+        }
+        this.#finish(false);
+    }
+
+    /**
+     * @param {boolean} [result]
+     * @returns {boolean}
+     */
+    #finish(result = true) {
+        this.#id = null;
+        this.#job?.catch(e => Context.logError(`${Job.name} failed.`, e));
+        if (typeof this.#resolver === 'function') this.#resolver(result);
+        this.#resolver = null;
+        this.#job = null;
+        return GLib.SOURCE_REMOVE;
+    }
+
+}
+
+export default class Jobs {
+
+    /** @type {Map<*, Set<Job>>?} */
+    #jobs = new Map();
+
+    destroy() {
+        if (!this.#jobs) return;
+        const jobs = this.#jobs.values();
+        this.#jobs = null;
+        for (const clientJobs of jobs) {
+            for (const job of clientJobs) job.destroy();
+        }
+    }
+
+    /**
+     * @param {*} client
+     * @param {number} [delay]
+     * @returns {Job}
+     */
+    new(client, delay = Delay.Idle) {
+        if (!this.#jobs) throw new Error(`${this.constructor.name} unable to create a new ${Job.name}.`);
+        return this.#add(client, new Job(job => this.#remove(client, job), delay));
+    }
+
+    /**
+     * @param {*} client
+     * @returns {boolean}
+     */
+    hasClient(client) {
+        return this.#jobs?.has(client) ?? false;
+    }
+
+    /**
+     * @param {*} client
+     * @returns {this}
+     */
+    removeAll(client) {
+        if (!client || !this.#jobs) return this;
+        const jobs = this.#jobs.get(client);
+        if (!jobs) return this;
+        this.#jobs.delete(client);
+        for (const job of jobs) job.destroy();
+        return this;
+    }
+
+    /**
+     * @param {*} client
+     * @param {Job} job
+     * @returns {Job}
+     */
+    #add(client, job) {
+        const jobs = this.#jobs?.get(client) ?? new Set();
+        jobs.add(job);
+        this.#jobs?.set(client, jobs);
+        return job;
+    }
+
+    /**
+     * @param {*} client
+     * @param {Job} job
+     */
+    #remove(client, job) {
+        const jobs = this.#jobs?.get(client);
+        if (!jobs) return;
+        if (jobs.has(job)) jobs.delete(job);
+        if (!jobs.size) this.#jobs?.delete(client);
+    }
+
+}
