b23d449b373e402e810f4c879954f66a60bd25e1
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-01-17 00:05:13 +0300

    main/context: now extends the shared Context class

--- extension/main/context.js
+++ extension/main/context.js
@@ -2,51 +2,23 @@
  * @typedef {import('resource:///org/gnome/shell/extensions/extension.js').Extension} Extension
  */
 
-import Settings from '../shared/settings.js';
-import Jobs from './context/jobs.js';
-import Signals from './context/signals.js';
+import SharedContext from '../shared/context.js';
 import Desktop from './context/desktop.js';
 import Modules from '../services/modules.js';
 import LauncherApi from '../services/launcherApi.js';
 
-const SETTINGS_SCHEMA_KEY = 'settings-schema';
+export default class Context extends SharedContext {
 
-export default class Context {
-
-    /** @type {Context?} */
+    /** @type {Context?} store another context instance in this class to optimize the call path */
     static #instance = null;
 
-    /** @type {Map<*, Map>?} a persistent cache to keep some data until the user's session ends or the extension gets disabled by the user */
-    static #sessionStorage = null;
-
-    /** @type {Context} */
+    /**
+     * @override
+     * @type {Context}
+     */
     static get instance() {
         if (!this.#instance) throw new Error(`${this.name} has no instance.`);
         return this.#instance;
-    }
-
-    /** @type {{[key: string]: *}?} */
-    static get metadata() {
-        return this.instance.#extension?.metadata;
-    }
-
-    /** @type {string?} */
-    static get path() {
-        return this.instance.#extension?.path ?? null;
-    }
-
-    /** @type {Jobs} */
-    static get jobs() {
-        const instance = this.instance;
-        instance.#jobs ??= new Jobs();
-        return instance.#jobs;
-    }
-
-    /** @type {Signals} */
-    static get signals() {
-        const instance = this.instance;
-        instance.#signals ??= new Signals();
-        return instance.#signals;
     }
 
     /** @type {Desktop} */
@@ -61,56 +33,6 @@ export default class Context {
         return this.instance.#launcherApi;
     }
 
-    /**
-     * @param {string?} [path]
-     * @returns {Settings?}
-     */
-    static getSettings(path) {
-        try {
-            const extension = this.instance.#extension;
-            if (!extension) return null;
-            const schemaId = path ? `${extension.metadata[SETTINGS_SCHEMA_KEY]}.${path}` : '';
-            const storage = this.getStorage(this.name);
-            if (storage.has(schemaId)) return storage.get(schemaId);
-            const settings = new Settings(extension.getSettings(schemaId));
-            storage.set(schemaId, settings);
-            return settings;
-        } catch (e) {
-            this.logError(`unable to load settings for the path ${path}.`);
-        }
-        return null;
-    }
-
-    /**
-     * @param {*} client
-     * @returns {Map}
-     */
-    static getStorage(client) {
-        if (!this.#instance) throw new Error(`${this.name} is invalid.`);
-        if (!client) throw new Error(`${this.name}.getStorage requires a client reference.`);
-        this.#sessionStorage ??= new Map();
-        const clientStorage = this.#sessionStorage.get(client) ?? new Map();
-        this.#sessionStorage.set(client, clientStorage);
-        return clientStorage;
-    }
-
-    /**
-     * @param {string?} [message]
-     * @param {*} [error]
-     */
-    static logError(message, error) {
-        console.error(`${this.metadata?.name ?? this.name} ${message ?? ''}`, error ?? '');
-    }
-
-    /** @type {Extension?} */
-    #extension = null;
-
-    /** @type {Jobs?} */
-    #jobs = null;
-
-    /** @type {Signals?} */
-    #signals = null;
-
     /** @type {Modules?} */
     #modules = null;
 
@@ -124,28 +46,18 @@ export default class Context {
      * @param {Extension} extension
      */
     constructor(extension) {
-        if (Context.#instance) throw new Error(`${Context.name} already has an instance.`);
-        if (!extension) throw new Error(`${Context.name} requires an instance of the extension class.`);
-        this.#extension = extension;
+        super(extension, () => this.#destroy());
         Context.#instance = this;
         if (!Context.desktop.isLocked) this.#initialize();
         else Context.jobs.new(this).destroy(() => this.#initialize());
     }
 
+    /**
+     * @override
+     */
     destroy() {
-        try {
-            this.#destroy();
-        } catch (e) {
-            Context.logError(`unable to destroy ${Context.name}.`, e);
-        } finally {
-            this.#modules = null;
-            this.#jobs = null;
-            this.#signals = null;
-            this.#launcherApi = null;
-            this.#desktop = null;
-            this.#extension = null;
-            Context.#instance = null;
-        }
+        super.destroy();
+        Context.#instance = null;
     }
 
     #initialize() {
@@ -153,33 +65,21 @@ export default class Context {
         this.#modules = new Modules();
     }
 
+    /**
+     * @returns {boolean}
+     */
     #destroy() {
-        this.#jobs?.removeAll(this);
-        this.#modules?.destroy();
-        this.#desktop?.destroy();
-        this.#jobs?.destroy();
-        this.#signals?.destroy();
-        this.#launcherApi?.destroy();
-        this.#cleanSessionStorage();
-    }
-
-    #cleanSessionStorage() {
-        const sessionStorage = Context.#sessionStorage;
-        if (!sessionStorage) return;
-        if (!Context.desktop.isLocked) {
-            sessionStorage.clear();
-            Context.#sessionStorage = null;
-            return;
+        const result = !Context.desktop.isLocked;
+        try {
+            this.#modules?.destroy();
+            this.#desktop?.destroy();
+            this.#launcherApi?.destroy();
+        } finally {
+            this.#modules = null;
+            this.#launcherApi = null;
+            this.#desktop = null;
         }
-        sessionStorage.delete(Context.name);
-        const clients = [...sessionStorage.keys()];
-        for (let i = 0, l = clients.length; i < l; ++i) {
-            const client = clients[i];
-            if (sessionStorage.get(client)?.size) continue;
-            sessionStorage.delete(client);
-        }
-        if (sessionStorage.size) return;
-        Context.#sessionStorage = null;
+        return result;
     }
 
 }
