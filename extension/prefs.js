e6bd4a3df670b9165fd34426ba4c2600849012d2
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-01-18 02:01:13 +0300

    prefs: instantiate the Context class

--- extension/prefs.js
+++ extension/prefs.js
@@ -1,15 +1,16 @@
 import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import { Event } from './shared/core/enums.js';
+import Context from './preferences/core/context.js';
 
 export default class extends ExtensionPreferences {
-
 
     /**
      * @override
      * @param {Adw.PreferencesWindow} window
      */
     fillPreferencesWindow(window) {
+        const runtime = new Context(this, window);
+        window.connect(Event.CloseRequest, () => runtime.destroy());
     }
-
-
 
 }
