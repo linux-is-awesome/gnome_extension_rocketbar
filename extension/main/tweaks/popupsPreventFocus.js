497a460f5450db915cd3152676ce0633a6526163
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-05-24 16:46:13 +0300

    tweaks/popupsPreventFocus: override global.stage.grab instead of  global.stage.set_key_focus to make it work again

--- extension/main/tweaks/popupsPreventFocus.js
+++ extension/main/tweaks/popupsPreventFocus.js
@@ -1,11 +1,17 @@
+import Clutter from 'gi://Clutter';
 import { SwitcherPopup } from 'resource:///org/gnome/shell/ui/switcherPopup.js';
 import Context from '../core/context.js';
+
+const FakePopupGrab = () => ({
+    get_seat_state: () => Clutter.GrabState.KEYBOARD,
+    dismiss: () => {}
+});
 
 export default class {
 
     constructor() {
-        Context.hooks.add(this, global.stage, global.stage.set_key_focus,
-            (_, actor) => actor instanceof SwitcherPopup || undefined, true);
+        Context.hooks.add(this, global.stage, global.stage.grab,
+            (_, actor) => actor instanceof SwitcherPopup ? FakePopupGrab() : undefined, true);
     }
 
     destroy() {
