a2cddc6fea0809a8d08909f3cc88462a2294daaa
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-12-19 11:04:28 +0300

    pages/notificationCounter: initial implementation

---
+++ extension/preferences/pages/notificationCounter.js
@@ -0,0 +1,50 @@
+import SettingsPage from './base/settingsPage.js';
+import { Config } from '../../shared/utils/config.js';
+import { ConfigOptions, ConfigField } from '../../shared/enums/notificationCounter.js';
+
+const PAGE_NAME = 'notificationCounter';
+
+export default class extends SettingsPage {
+
+    /** @type {Config} */
+    #config = Config(this, ConfigField, (settingsKey, value) => this.#handleConfig(settingsKey, value), ConfigOptions);
+
+    constructor() {
+        super(PAGE_NAME, () => this.#initialize(), ConfigOptions.path);
+    }
+
+    #initialize() {
+        for (const key in ConfigField) {
+            const settingsKey = ConfigField[key];
+            this.#handleConfig(settingsKey, this.#config[key]);
+        }
+    }
+
+    /**
+     * @param {string} settingsKey
+     * @param {*} value
+     */
+    #handleConfig(settingsKey, value) {
+        switch (settingsKey) {
+            case ConfigField.centerClock:
+            case ConfigField.hideEmpty:
+                this.setBoolean(settingsKey, value);
+                break;
+            case ConfigField.fontSize:
+            case ConfigField.maxCount:
+            case ConfigField.offset:
+            case ConfigField.roundness:
+                this.setNumber(settingsKey, value);
+                break;
+            case ConfigField.colorEmpty:
+            case ConfigField.colorEmptyDnd:
+            case ConfigField.colorNotEmpty:
+            case ConfigField.colorNotEmptyDnd:
+            case ConfigField.textColor:
+            case ConfigField.textColorDnd:
+                this.setColor(settingsKey, value);
+                break;
+        }
+    }
+
+}
