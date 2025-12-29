ec86cf1af453bb10e2290c6392f37ec5483c4160
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2025-12-29 11:48:08 +0300

    base/settingsPage: handle more widget types

--- extension/preferences/pages/base/settingsPage.js
+++ extension/preferences/pages/base/settingsPage.js
@@ -3,6 +3,8 @@
  */
 
 import Gdk from 'gi://Gdk';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
 import Page from './page.js';
 import Context from '../../core/context.js';
 import { Event } from '../../../shared/enums/general.js';
@@ -31,7 +33,6 @@ export default class SettingsPage extends Page {
         this.#settings = Context.getSettings(settingsPath);
     }
 
-
     /**
      * @param {string} settingsKey
      * @param {boolean} value
@@ -40,10 +41,28 @@ export default class SettingsPage extends Page {
         if (!this.#settings) return;
         value ??= false;
         if (typeof value !== 'boolean') return;
-        const widget = this.getSwitchRow(settingsKey);
-        if (widget.get_active() !== value) widget.set_active(value);
-        if (this.#connectedSettings.has(settingsKey)) return;
-        widget.connect(Event.Active, () => this.#settings?.set(settingsKey, widget.get_active()));
+        const widget = this.getObject(settingsKey);
+        const isConnected = this.#connectedSettings.has(settingsKey);
+        if (widget instanceof Gtk.Switch) {
+            if (widget.get_active() !== value) widget.set_active(value);
+            const row = this.getObject(`${settingsKey}-row`);
+            const arrow = this.getObject(`${settingsKey}-row-arrow`);
+            if (row instanceof Adw.ActionRow) row.set_activatable(value);
+            if (arrow instanceof Gtk.Image) arrow.set_sensitive(value);
+            if (isConnected) return;
+            widget.connect(Event.ActiveChanged, () =>
+            this.settings.set(settingsKey, widget.get_active()));
+        } else if (widget instanceof Adw.SwitchRow) {
+            if (widget.get_active() !== value) widget.set_active(value);
+            if (isConnected) return;
+            widget.connect(Event.ActiveChanged, () =>
+            this.settings.set(settingsKey, widget.get_active()));
+        } else if (widget instanceof Adw.ExpanderRow) {
+            if (widget.get_enable_expansion() !== value) widget.set_enable_expansion(value);
+            if (isConnected) return;
+            widget.connect(Event.EnableExpansionChanged, () =>
+            this.settings.set(settingsKey, widget.get_enable_expansion()));
+        }
         this.#connectedSettings.add(settingsKey);
     }
 
@@ -58,7 +77,8 @@ export default class SettingsPage extends Page {
         const widget = this.getSpinRow(settingsKey);
         if (widget.get_value() !== value) widget.set_value(value);
         if (this.#connectedSettings.has(settingsKey)) return;
-        widget.connect(Event.ValueChanged, () => this.#settings?.set(settingsKey, widget.get_value()));
+        widget.connect(Event.ValueChanged, () =>
+        this.#settings?.set(settingsKey, widget.get_value()));
         this.#connectedSettings.add(settingsKey);
     }
 
@@ -77,7 +97,27 @@ export default class SettingsPage extends Page {
             widget.set_rgba(rgba);
         }
         if (this.#connectedSettings.has(settingsKey)) return;
-        widget.connect(Event.RgbaChanged, () => this.#settings?.set(settingsKey, widget.get_rgba().to_string()));
+        widget.connect(Event.RgbaChanged, () =>
+        this.#settings?.set(settingsKey, widget.get_rgba().to_string()));
+        this.#connectedSettings.add(settingsKey);
+    }
+
+    /**
+     * @param {string} settingsKey
+     * @param {string} value
+     * @param {string[]} options
+     */
+    setOption(settingsKey, value, options) {
+        if (!this.#settings) return;
+        value ??= '';
+        if (typeof value !== 'string') return;
+        const optionIndex = options.indexOf(value);
+        if (optionIndex < 0) return;
+        const widget = this.getComboRow(settingsKey);
+        if (widget.get_selected() !== optionIndex) widget.set_selected(optionIndex);
+        if (this.#connectedSettings.has(settingsKey)) return;
+        widget.connect(Event.SelectedItemChanged, () =>
+        this.#settings?.set(settingsKey, options[widget.get_selected()]));
         this.#connectedSettings.add(settingsKey);
     }
 
