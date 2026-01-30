c19831dd730e5b0ac7104eeb03f85834ae669fc6
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2026-01-31 00:09:05 +0300

    tweaks/overviewClicks: only use capture event to register press, validate all press target types, special behavior for scroll views

--- extension/main/tweaks/overviewClicks.js
+++ extension/main/tweaks/overviewClicks.js
@@ -1,11 +1,11 @@
 /**
- * @typedef {import('resource:///org/gnome/shell/ui/overviewControls.js').ControlsManager} ControlsManager
- * @typedef {import('resource:///org/gnome/shell/ui/searchController.js').SearchController} SearchController
  * @typedef {import('resource:///org/gnome/shell/ui/windowPreview.js').WindowPreview} WindowPreview
  */
 
 import Clutter from 'gi://Clutter';
+import St from 'gi://St';
 import { WindowPreview } from 'resource:///org/gnome/shell/ui/windowPreview.js';
+import { WorkspacesDisplay } from 'resource:///org/gnome/shell/ui/workspacesView.js';
 import { Overview, MainLayout } from '../core/shell.js';
 import Context from '../core/context.js';
 import { ActorPressHandler } from '../ui/base/actorPressHandler.js';
@@ -16,27 +16,48 @@ export default class {
     /** @type {ActorPressHandler?} */
     #pressHandler = null;
 
+    /** @type {[x: number, y: number]?} */
+    #pointerPosition = null;
+
     constructor() {
         if (!MainLayout.overviewGroup || typeof Overview.toggle !== 'function') return;
         this.#pressHandler = new ActorPressHandler((...args) => this.#longPress(...args));
         Context.signals.add(this, [MainLayout.overviewGroup,
             Event.Leave, () => this.#pressHandler?.release(),
             Event.ButtonRelease, () => this.#pressHandler?.release((...args) => this.#click(...args)),
-            Event.ButtonPress, (_, event) => this.#pressHandler?.press(event, () =>
-                Overview.visible ? Clutter.EVENT_STOP :
-                (this.#pressHandler?.release(), Clutter.EVENT_PROPAGATE)),
             Event.Captured, (_, event) => this.#pressHandler?.press(event, (button, target) =>
-                (button === Clutter.BUTTON_MIDDLE ||
-                    (button === Clutter.BUTTON_PRIMARY &&
-                        !!(event.get_state() & Clutter.ModifierType.CONTROL_MASK))) &&
-                Overview.visible && target instanceof WindowPreview ? Clutter.EVENT_STOP :
-                (this.#pressHandler?.release(), Clutter.EVENT_PROPAGATE))]);
+                                          this.#getPressResult(event, button, target))]);
     }
 
     destroy() {
         Context.signals.removeAll(this);
         this.#pressHandler?.destroy();
         this.#pressHandler = null;
+        this.#pointerPosition = null;
+    }
+
+    /**
+     * @param {Clutter.Event} event
+     * @param {number} button
+     * @param {Clutter.Actor} target
+     * @returns {boolean}
+     */
+    #getPressResult(event, button, target) {
+        this.#pointerPosition = null;
+        if (!Overview.visible) return Clutter.EVENT_PROPAGATE;
+        if (target instanceof WindowPreview) {
+            const isCtrlPressed = !!(event.get_state() & Clutter.ModifierType.CONTROL_MASK);
+            if (button === Clutter.BUTTON_MIDDLE ||
+               (button === Clutter.BUTTON_PRIMARY && isCtrlPressed)) return Clutter.EVENT_STOP
+        } else if (target instanceof St.ScrollView) {
+            if (button !== Clutter.BUTTON_PRIMARY) return Clutter.EVENT_STOP;
+            const [x, y] = global.get_pointer();
+            this.#pointerPosition = [x, y];
+            return Clutter.EVENT_PROPAGATE;
+        } else if (target === MainLayout.overviewGroup ||
+                   target instanceof WorkspacesDisplay) return Clutter.EVENT_STOP;
+        this.#pressHandler?.release(); 
+        return Clutter.EVENT_PROPAGATE;
     }
 
     /**
@@ -45,6 +66,7 @@ export default class {
      */
     #longPress(event, target) {
         this.#pressHandler?.release();
+        if (!this.#validatePointerPosition()) return;
         if (target instanceof WindowPreview) return this.#closeWindow(target);
         if (event.get_button() === Clutter.BUTTON_MIDDLE) return;
         this.#toggleOverview(false);
@@ -55,6 +77,7 @@ export default class {
      * @param {Clutter.Actor} target
      */
     #click(event, target) {
+        if (!this.#validatePointerPosition()) return;
         if (target instanceof WindowPreview) return this.#closeWindow(target);
         const button = event.get_button();
         if (button === Clutter.BUTTON_MIDDLE) return;
@@ -72,6 +95,17 @@ export default class {
         const overviewControls = Overview._overview?._controls;
         if (typeof overviewControls?._toggleAppsPage !== 'function') return;
         overviewControls._toggleAppsPage();
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    #validatePointerPosition() {
+        if (!this.#pointerPosition) return true;
+        const [x, y] = this.#pointerPosition;
+        const [currentX, currentY] = global.get_pointer();
+        this.#pointerPosition = null;
+        return x === currentX && y === currentY;
     }
 
     /**
